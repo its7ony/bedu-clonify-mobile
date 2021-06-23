5cab7ca1593f744b8e910de0dbfb06d7aa900a22
Antonio Labra <antonio_labra@hotmail.com>
2021-06-23 18:17:56 -0500

    Agregando Carpetas para trabajar y archivos.kt

---
+++ S1/s1.kt
@@ -0,0 +1,35 @@
+package Postwork
+fun main() {
+//Requisitos del postwork
+// Abstraer el concepto de ciclos y condicionales.
+//Manejar de forma básica el IDE.
+//Aplicar variables y realizar operaciones sobre ellas.
+//Desarrollo
+// Elementos del perfil del usuario para registrarse en Clonifi
+    var correoElectronico="jorge@gmail.com"
+    var contraseña = "Jorge123"
+    var nombre = "Jorge"
+// Fecha de nacimiento formato mm/dd/aa
+    var mes= 04f
+    var dia= 12f
+    var año= 1994f
+//Elección de genero
+    var sexo = "No binario"
+//Primeros pasos en el proyecto; utilizar el condicional if para imprimir en pantalla reproducir
+    println("Reproducir")
+    var reproducir = readLine()
+    if (reproducir == "y") {
+        println("☻Reproduciendo☻")
+        println("██████")
+        println("█████")
+        println("████")
+        println("█████")
+        println("████████")
+    } else {
+        println("pausa")
+    }
+}
+
+
+
+
