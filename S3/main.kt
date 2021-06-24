e976808ac2c274c8f9682c6b5cee7bc1f5582f1a
Oscar <krz.oficial@gmail.com>
2021-06-24 00:23:17 -0500

    Pueba

---
+++ S3/main.kt
@@ -0,0 +1,9 @@
+package proyecto
+
+fun main(){
+
+    val listaArtista= listOf(Artista("Artista1",3,10))
+    val listaAlbum= listOf(Album("Artista1","Album1",10))
+    val listaCancion= listOf(Cancion("Artista1","Album1","Cancion1",231))
+    
+}
---
+++ S3/s3.kt
@@ -0,0 +1,25 @@
+package proyecto
+
+
+/*Reto final - Añadiendo POO al proyecto
+OBJETIVO
+
+Diseñar las entidades requeridas de acuerdo a la Programación Orientada a Objetos.
+Implementar objetos en el proyecto final.
+Adaptar el código previamente desarrollado a clases.
+*/
+
+
+class Artista(nombreArtista: String, totalAlbunes: Int,totalCanciones:Int){
+    
+}
+
+class Album(val nombreArtista: String, val nombreAlbum: String,val cantidadCanciones: Int){
+
+
+}
+
+class Cancion(nombreArtista: String, nombreAlbum: String, nombreCancion:String, duracion:Int){
+
+
+}
